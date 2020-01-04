dc92cdd895d259d8dfc5d07fc51adfc7dbe7e5a7
Jerry <xzhou@scripps.edu>
2020-01-03 20:36:57 -0800

    Pending refactor starting script

--- src/index.py
+++ src/index.py
@@ -14,14 +14,14 @@ if web_settings.INCLUDE_DOCS and not os.path.exists(web_settings.DOCS_STATIC_PAT
 
 APP_LIST = web_settings.generate_app_list()
 
-if options.debug:
-    APP_LIST += [
-        (r"/widget/(.*)", StaticFileHandler, {'path': os.path.join(web_settings.STATIC_PATH, 'widget')}),
-        (r"/?(.*)", StaticFileHandler, {'path': web_settings.DOCS_STATIC_PATH}),
-    ]
+# if options.debug:
+#     APP_LIST += [
+#         (r"/widget/(.*)", StaticFileHandler, {'path': os.path.join(web_settings.STATIC_PATH, 'widget')}),
+#         (r"/?(.*)", StaticFileHandler, {'path': web_settings.DOCS_STATIC_PATH}),
+#     ]
 
 if __name__ == '__main__':
     # set debug level on app settings
-    web_settings.set_debug_level(options.debug)
+    # web_settings.set_debug_level(options.debug)
     main(APP_LIST, debug_settings={"static_path": web_settings.STATIC_PATH, "debug": True},
          sentry_client_key=web_settings.SENTRY_CLIENT_KEY)
